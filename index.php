fd79af5bd7294f8acc39e3329624397588ec7d2a
Michael <ko4erov@mail.ru>
2018-12-28 00:00:05 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+	function greeting($name, $dayOfWeek) {
+		switch ($dayOfWeek) {
+			case '1': case '2': case '3': case '4': case '5':
+				echo "Привет, $name! Хорошего и продуктивного рабочего дня!";
+				break;
+			case '6': case '7':
+				echo "Привет, $name! Желаю вам хорошо отдохнуть в этот день!";
+				break;
+			default:
+				echo "Не расслышал день недели...";
+				break;
+		}
+	}
+	greeting("Феофан", "3");
+ ?>
